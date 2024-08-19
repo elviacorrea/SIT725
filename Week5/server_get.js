60b1f766e903235c7a21e60747225303966a2017
elvia-correa <correa.victor@learner.manipal.edu>
2024-08-19 22:12:30 +1000

    week5

---
+++ Week5/server_get.js
@@ -0,0 +1,21 @@
+// server_get.js
+const express = require('express');
+const userRoutes = require('./routers/userRoutes');
+const { connectToDB } = require('./models/userModel');
+
+const app = express();
+let port = process.env.PORT || 3000;
+
+// Middleware to handle JSON and URL-encoded requests
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/', userRoutes);
+
+// Connect to the database and start the server
+app.listen(port, () => {
+    console.log('Express server started on port', port);
+    connectToDB();  // Initialize the database connection
+});
